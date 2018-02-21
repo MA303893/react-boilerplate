eb09c1064d927ab3425df10ebc27717db65e4416
Manish Singh <manish.singh@jifflenow.com>
2018-02-21 20:25:28 +0530

    BoilerPlate commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+import {Header} from 'semantic-ui-react'
+
+export default class AppHeader extends Component {
+  render() {
+    return(
+      <Header as='h2'>Get your weather forecast here!</Header>
+    )
+  }
+}
